f2291f20922a43200813d50b08996228ce89cbe5
Denisse Velázquez <ldvm.336@gmail.com>
2024-09-11 14:12:02 -0600

    Update 8269fd3e-b90e-462b-a194-d77cac715a6a.py

--- 8269fd3e-b90e-462b-a194-d77cac715a6a.py
+++ 8269fd3e-b90e-462b-a194-d77cac715a6a.py
@@ -1,5 +1,3 @@
-# ¡Empecemos!
-
 # El objetivo de este proyecto es desarrollar un modelo que pueda analizar el comportamiento de los clientes y recomendar uno de los nuevos planes de Megaline: Smart o Ultra.
 
 # Contenido:
