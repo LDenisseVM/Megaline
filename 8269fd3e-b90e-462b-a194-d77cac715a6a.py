fe30b0b5bf14d94333f275be65428f7add933627
Denisse Velázquez <ldvm.336@gmail.com>
2024-09-11 14:11:47 -0600

    Clean - Without comments

--- 8269fd3e-b90e-462b-a194-d77cac715a6a.py
+++ 8269fd3e-b90e-462b-a194-d77cac715a6a.py
@@ -1,45 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # ¡Hola, Denisse!  
-# 
-# Mi nombre es Carlos Ortiz, soy code reviewer de Practicum y voy a revisar el proyecto que acabas de desarrollar.
-# 
-# Cuando vea un error la primera vez, lo señalaré. Deberás encontrarlo y arreglarlo. La intención es que te prepares para un espacio real de trabajo. En un trabajo, el líder de tu equipo hará lo mismo. Si no puedes solucionar el error, te daré más información en la próxima ocasión. 
-# 
-# Encontrarás mis comentarios más abajo - **por favor, no los muevas, no los modifiques ni los borres**.
-# 
-# ¿Cómo lo voy a hacer? Voy a leer detenidamente cada una de las implementaciones que has llevado a cabo para cumplir con lo solicitado. Verás los comentarios de esta forma:
-# 
-# <div class="alert alert-block alert-success">
-# <b>Comentario del revisor</b> <a class="tocSkip"></a>
-#     
-# Si todo está perfecto.
-# </div>
-# 
-# 
-# <div class="alert alert-block alert-warning">
-# <b>Comentario del revisor</b> <a class="tocSkip"></a>
-#     
-# Si tu código está bien pero se puede mejorar o hay algún detalle que le hace falta.
-# </div>
-# 
-# 
-# <div class="alert alert-block alert-danger">
-#     
-# <b>Comentario del revisor</b> <a class="tocSkip"></a>
-#     
-#     
-# Si de pronto hace falta algo o existe algún problema con tu código o conclusiones.
-# </div>
-# 
-# 
-# Puedes responderme de esta forma: 
-# 
-# <div class="alert alert-block alert-info">
-# <b>Respuesta del estudiante</b> <a class="tocSkip"></a>
-# </div>
-# 
 # ¡Empecemos!
 
 # El objetivo de este proyecto es desarrollar un modelo que pueda analizar el comportamiento de los clientes y recomendar uno de los nuevos planes de Megaline: Smart o Ultra.
@@ -52,7 +10,6 @@
 #     
 #     Creación del mejor modelo
 
-# In[1]:
 
 
 import pandas as pd
@@ -70,26 +27,11 @@ from sklearn.metrics import accuracy_score
 
 # Cargamos los datos y los revisamos
 
-# In[2]:
 
 
 df = pd.read_csv('/datasets/users_behavior.csv')
 
-
-# <div class="alert alert-block alert-success">
-# <b>Comentario del revisor</b> <a class="tocSkip"></a>
-#     
-# Buen trabajo con la importación de datos.
-# </div>
-
-# In[3]:
-
-
 df.head()
-
-
-# In[4]:
-
 
 df.info()
 
@@ -98,33 +40,18 @@ df.info()
 
 # Dividimos los datos en 3, de entrenamiento, de validación y de prueba:
 
-# In[5]:
-
 
 features = df.drop(['is_ultra'], axis=1)
 target = df['is_ultra']
 
 features_train, features_valid, target_train, target_valid = train_test_split(features, target, test_size=.40, random_state=12345)
 
-
-# In[6]:
-
-
 features_test, features_valid, target_test, target_valid = train_test_split(features_valid, target_valid, test_size=.50, random_state=12345)
 
-
-# <div class="alert alert-block alert-success">
-# <b>Comentario del revisor</b> <a class="tocSkip"></a>
-#     
-# Has hecho bien con el split de los datos.
-# </div>
 
 # Haremos distintas pruebas para ver que modelo es más exacto y con que parámetros 
 
 # Bosque aleatorio:
-
-# In[7]:
-
 
 best_est = 0
 best_depth = 0
@@ -147,9 +74,6 @@ print("Accuracy del mejor modelo en el conjunto de validación:", best_score, ",
 
 # Arbol de decisión:
 
-# In[8]:
-
-
 best_depth = 0
 best_score = 0
 best_tree= None
@@ -167,9 +91,6 @@ print("Accuracy del mejor modelo en el conjunto de validacion:", best_score, ", 
 
 # Regresión logica:
 
-# In[9]:
-
-
 model = LogisticRegression(random_state=12345, solver='liblinear')
 model.fit(features_train, target_train) 
 score_valid = model.score(features_valid, target_valid) 
@@ -181,17 +102,11 @@ print("Accuracy del modelo de regresión logística en el conjunto de entrenamie
 
 # Asi que comprobaremos la calidad del modelo de bosque aleatorio usando el conjunto de prueba
 
-# In[10]:
-
-
 best_preds = best_rf.predict(features_test)
 print(accuracy_score(best_preds, target_test))
 
 
 # A continuación hacemos una prueba de cordura al modelo:
-
-# In[11]:
-
 
 dummy_classifier = DummyClassifier(strategy="most_frequent")
 
@@ -208,21 +123,3 @@ print("Precisión del modelo Dummy:", accuracy)
 
 # Con esto, en este proyecto creamos un modelo que usando un bosque aleatorio, analiza el comportamiento de los clientes y recomienda uno de los nuevos planes de Megaline: Smart o Ultra.
 # Elegimos este modelo, comparando la exactitud de varios modelos, y resultando este el mejor, fue el escogido
-
-# <div class="alert alert-block alert-danger">
-# 
-# # Comentarios generales #2
-# <b>Comentario del revisor</b> <a class="tocSkip"></a>
-#     
-# Bien, Denisse. Nos quedan algunos elementos más de forma que de fondo por resolver. He dejado comentarios adicionales para ello. Agrega una conclusión que resuma lo realizado, lo encontrado a partir de lo realizado y responda los objetivos del proyecto.
-#     
-# </div>
-
-# <div class="alert alert-block alert-success">
-# 
-# # Comentarios generales #3
-# <b>Comentario del revisor</b> <a class="tocSkip"></a>
-#     
-# Todo corregido. Has aprobado un nuevo proyecto.
-#     
-# </div>
